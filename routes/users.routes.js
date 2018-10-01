c6baed379d98a543961983d229876b42bd4901be
MartaSM <solermoran@gmail.com>
2018-10-01 22:11:11 +0200

    Authenticate middleware

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const users = require('../controllers/users.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/', users.create);
-router.get('/:id', users.detail);
+router.get('/:id', authMiddleware.isAuthenticated, users.detail);
 
 router.post(
-  '/:id/edit', 
-  //insert middlewares here- authenticated, owner and photo upload
-  users.edit
+'/:id/edit',
+//insert middlewares here- authenticated, owner and photo upload
+users.edit
 );
 
 module.exports = router;
