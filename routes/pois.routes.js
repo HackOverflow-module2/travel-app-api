3447cb3f88eafab7e32cffa1c6cd555e2cfe7a02
MartaSM <solermoran@gmail.com>
2018-10-12 16:20:24 +0200

    Multer - chaange file name

--- routes/pois.routes.js
+++ routes/pois.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pois = require('../controllers/pois.controller');
 const Poi = require('../models/poi.model');
+
 const uploader = require('../config/multer.config');
 const authMiddleware = require('../middlewares/auth.middleware');
 
@@ -10,9 +11,11 @@ router.get('/list', pois.list);
 router.post(
   '/', 
   authMiddleware.isAuthenticated,
-  uploader.array('images'),
+  uploader.array('file'),
   pois.create
 );
+
+
 router.get('/:id', pois.detail);
 
 router.post(
