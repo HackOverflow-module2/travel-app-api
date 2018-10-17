74eb20cdf1d1fac67f00e3e83fb417ce302057da
Georgina <paceygeorgina@gmail.com>
2018-10-17 21:40:06 +0200

    cloudinary

--- routes/trips.routes.js
+++ routes/trips.routes.js
@@ -4,7 +4,7 @@ const trips = require('../controllers/trips.controller');
 const Trip = require('../models/trip.model');
 const authMiddleware = require('../middlewares/auth.middleware');
 const uploader = require('../config/multer.config');
-const uploadCloud = require('../config/cloudinary.js');
+const uploadCloud = require('../config/cloudinary.config');
 
 router.get('/list', trips.list);
 router.post('/',
